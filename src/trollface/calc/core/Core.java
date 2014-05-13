be77a78c382c99c996be2c053d0ee36397becd18
David Kamberský <david.kambersky@gmail.com>
2014-05-13 14:34:57 +0200

    Initial commit

---
+++ src/trollface/calc/core/Core.java
@@ -0,0 +1,19 @@
+/** The Core.java class responsible for running the damn thing 
+ *
+ * @author xCabbage [github.com/xcabbage]
+ *
+ * @info for the ChemCalc project [github.com/trollface-studios/chemcalculator]
+ *      created 13. 5. 2014 14:12:50
+ */
+
+package trollface.calc.core;
+
+/**
+ * @author David
+ * 
+ */
+public class Core {
+	public static void main(String[] args) {
+		System.out.println("sup");
+	}
+}
