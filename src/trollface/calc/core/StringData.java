34228eb86a1475bb3c276e676f8079c66069ee92
Nina Caisova <nina.cais@gmail.com>
2014-07-05 20:12:34 +0200

    elements added

---
+++ src/trollface/calc/core/StringData.java
@@ -0,0 +1,14 @@
+package trollface.calc.core;
+
+public class StringData {
+	String name, fullname;
+	double molarmass;
+	String type, electronegativity1;
+	int number;
+	double electronegativity;
+	double density;
+	
+	
+	
+
+}
