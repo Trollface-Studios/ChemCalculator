b3fda0483a716d77cc3d673f5d1edf7a5cc94e3e
Nina Caisova <nina.cais@gmail.com>
2014-09-02 16:22:30 +0200

    Class Textbox added

---
+++ src/trollface/calc/core/Textbox.java
@@ -0,0 +1,32 @@
+package trollface.calc.core;
+
+public class Textbox {
+	
+	String name, units;
+	int poradi;
+	
+	public Textbox(String Type){
+		switch(Type){
+		case "molarmass":
+			name = "Molar mass";
+			units = "g/mol";
+			poradi = 1;
+			break;
+		case "mass":
+			name = "Mass";
+			units = "g";
+			poradi = 2;
+			break;
+		case "n":
+			name = "n";
+			units = "mol";
+			poradi = 3;
+			break;
+		case "concetration":
+			name = "Concentration";
+			units = "mol/l";
+			poradi = 4;
+		}
+	}
+
+}
