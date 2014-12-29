c99c46dd1c3625336913a081b64a320d23830ce2
David Kamberský <david.kambersky@gmail.com>
2014-12-29 13:27:45 +0100

    Equation framework WIP

---
+++ src/trollface/calc/equations/Equation.java
@@ -0,0 +1,11 @@
+package trollface.calc.equations;
+
+public class Equation implements EquationInterface {
+
+	@Override
+	public double calculate(int mode, double... is) {
+
+		return 0;
+	}
+
+}
---
+++ src/trollface/calc/equations/EquationInterface.java
@@ -0,0 +1,8 @@
+package trollface.calc.equations;
+
+public interface EquationInterface {
+	String friendlyName = "New Equation";
+	String[] modes = null;
+
+	public double calculate(int mode, double... arguments);
+}
