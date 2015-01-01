b29e69569ab96edf87d76e3d3fd657f37c109bc5
Matyáš Bubna-Litic <matyas.bubna@email.cz>
2015-01-01 23:25:31 +0100

    Conc_calc_2

---
+++ src/trollface/calc/equations/Concentration.java
@@ -0,0 +1,33 @@
+package trollface.calc.equations;
+
+public class Concentration extends Equation {
+
+	public Concentration() {
+
+		
+		friendlyName = "Concentration";
+
+
+		modes = new Properties[][] {
+				{ Properties.con1, Properties.mol1, Properties.vol1 },
+				{ Properties.mol1, Properties.vol1, Properties.con1 },
+				{ Properties.vol1, Properties.mol1, Properties.con1 } };
+
+	}
+
+	@Override
+	public String calculate(int mode, double... arguments) {
+		switch (mode) {
+		case 0:
+			return arguments[1] / arguments[0] + " mol dm-3";
+		case 1:
+			return arguments[0] * arguments[1] + " mol";
+		case 2:
+			return arguments[0] * arguments[1] + " dm-3";
+		default:
+			System.out.printf("ERROR: WRONG MODE SPECIFIED: %d\n", mode);
+			return "ERROR. LOL";
+		}
+
+	}
+}
