af23009e1f13e634357bb2dc20be5794f2ec8aea
Matyáš Bubna-Litic <matyas.bubna@email.cz>
2015-05-22 00:10:58 +0200

    Revert "minor updates"
    This reverts commit 9b623d3a1592108d4cfe98870cf46a0b0d31a56e.

--- src/trollface/calc/equations/HHpH.java
+++ src/trollface/calc/equations/HHpH.java
@@ -6,17 +6,22 @@ public class HHpH extends Equation {
 
 		friendlyName = "Henderson-Hasselbalch (pH-pKa)";
 
-		modes = new Properties[][] {
-				{ Properties.ph, Properties.cacid, Properties.cbase, Properties.pka } } ;
-
+		/*
+		 * modes = new Properties[][] { { Properties., Properties., Properties.
+		 * }, { Properties., Properties., Properties. }, { Properties.,
+		 * Properties., Properties. } };
+		 */
 	}
 
 	@Override
 	public String calculate(int mode, double... arguments) {
 		switch (mode) {
 		case 0:
-//				return arguments[3] + arguments[1] + arguments[2] + " ";
-
+			return arguments[1] / arguments[0] + " ";
+		case 1:
+			return arguments[0] * arguments[1] + " ";
+		case 2:
+			return arguments[0] * arguments[1] + " ";
 		default:
 			System.out.printf("ERROR: WRONG MODE SPECIFIED: %d\n", mode);
 			return "ERROR. LOL";
--- src/trollface/calc/equations/Molarity.java
+++ src/trollface/calc/equations/Molarity.java
@@ -12,18 +12,18 @@ public class Molarity extends Equation {
 				{ Properties.con1, Properties.mol1, Properties.vol1 },
 				{ Properties.mol1, Properties.vol1, Properties.con1 },
 				{ Properties.vol1, Properties.mol1, Properties.con1 } };
- 
+
 	}
 
 	@Override
 	public String calculate(int mode, double... arguments) {
 		switch (mode) {
 		case 0:
-			return arguments[1] / arguments[0] + " mol dm^-3";
+			return arguments[1] / arguments[0] + " mol dm-3";
 		case 1:
 			return arguments[0] * arguments[1] + " mol";
 		case 2:
-			return arguments[0] * arguments[1] + " dm^3";
+			return arguments[0] * arguments[1] + " dm-3";
 		default:
 			System.out.printf("ERROR: WRONG MODE SPECIFIED: %d\n", mode);
 			return "ERROR. LOL";
